d8aeb9daa6c3364d3db14769ae167f561e71dfea
Eduardo Tavares <edutav@gmail.com>
2019-08-30 20:44:49 -0400

    inicio de teste com função e o this.props

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import { render } from 'react-dom'
 
 class Button extends Component {
   render() {
-    return <a href=''>Salvar</a>
+    return (
+      <a href='#' onClick={this.props.onClick}>
+        {this.props.title}
+      </a>
+    )
   }
 }
 
 class App extends Component {
+  clicar() {
+    console.log('Botão clicado')
+  }
   render() {
     return (
       <Fragment>
         <h1>Olá Mundo!!</h1>
-        <Button />
+        <Button title='Enviar' onClick={this.clicar} />
       </Fragment>
     )
   }
